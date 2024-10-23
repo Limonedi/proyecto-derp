5ac2ce4fa0726b7e8f368c744cdffbe0eb4cd57e
Limonedi <holuloldoce@gmail.com>
2024-10-23 11:53:05 -0500

    commit inicial

---
+++ src/main/java/com/perks/DerpApplication.java
@@ -0,0 +1,13 @@
+package com.perks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerpApplication.class, args);
+	}
+
+}
